36113611979020cda31999388a8c4710eccdaacd
honorine <honorineru@gmail.com>
2024-02-08 16:37:54 +0100

    [FEAT] sort by location

--- resources/js/Pages/Events/Show.vue
+++ resources/js/Pages/Events/Show.vue
@@ -15,6 +15,10 @@ const props = defineProps({
         type: String,
         required: true,
     },
+    locations: {
+        type: Array,
+        required: true,
+    },
     isUserAttached: {
         type: Boolean,
         required: true,
@@ -32,10 +36,6 @@ const props = defineProps({
         required: true,
     },
 });
-
-var adresse = '569 Eldred Vista Willville, SC 76641-8463';
-var ville = adresse.split(',')[0].trim();
-console.log(ville);
 
 
 </script>
@@ -56,7 +56,7 @@ console.log(ville);
 
                 <div class="w-full md:w-1/2 p-6 flex flex-col">
                     <img src="https://picsum.photos/1000?random=0" alt=""
-                         class="mx-auto w-96 h-96 object-cover rounded-lg shadow-md"/>
+                        class="mx-auto w-96 h-96 object-cover rounded-lg shadow-md" />
                 </div>
 
                 <div class="w-full md:w-1/2 p-6 space-y-2">
@@ -82,8 +82,7 @@ console.log(ville);
                     <p class="mt-2 text-gray-600">
                         Participants : {{ props.userCount }}
                     </p>
-                    <a v-if="!props.isUserAttached" :href="`/attach/events/${props.event.id}/users/${props.userId}`"
-                       class="mt-4 block w-full px-6 py-2 text-xs font-medium leading-6 text-center text-white uppercase transition bg-gray-700 rounded shadow
+                    <a v-if="!props.isUserAttached" :href="`/attach/events/${props.event.id}/users/${props.userId}`" class="mt-4 block w-full px-6 py-2 text-xs font-medium leading-6 text-center text-white uppercase transition bg-gray-700 rounded shadow
                     hover:shadow-lg hover:bg-gray-600 focus:outline-none">
                         S'inscrire
                     </a>
@@ -110,7 +109,7 @@ console.log(ville);
                     </div>
 
 
-                    <h2>Événements connexes :</h2>
+                    <h2>Événements avec une ou plusieurs catégorie.s commune.s :</h2>
 
                     <div v-if="props.relatedEvents && props.relatedEvents.length > 0">
 
@@ -119,7 +118,7 @@ console.log(ville);
                                 <a :href="route('events.show', relatedEvent)" class="block overflow-hidden">
                                     <div class="group">
                                         <img src="https://picsum.photos/1000?random=0" alt=""
-                                             class="h-[200px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[200px]"/>
+                                            class="h-[200px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[200px]" />
                                         <h3
                                             class="mt-2 text-sm text-gray-700 group-hover:underline group-hover:underline-offset-4">
                                             {{ relatedEvent.title }}
@@ -130,17 +129,60 @@ console.log(ville);
                                             <span class="tracking-wider text-gray-900"> {{ relatedEvent.price }}$</span>
                                         </p>
                                         <p>
-                                        <span class="tracking-wider text-red-800 text-xs hover:underline hover:underline-offset-4">
-                                            {{ relatedEvent.location }} {{relatedEvent.attachedCategories }}
-                                        </span>
+                                            <span
+                                                class="tracking-wider text-red-800 text-xs hover:underline hover:underline-offset-4">
+                                                {{ relatedEvent.location }} {{ relatedEvent.attachedCategories }}
+                                            </span>
                                         </p>
                                     </div>
                                 </a>
                             </li>
                         </ul>
                     </div>
-                    <p v-else> Aucun événement connexe trouvé </p>
+                    <p v-else> Aucun événement avec la même catégorie trouvé </p>
+
+
+
+
+
+                    <h2>Événements dans la même ville :</h2>
+
+                    <div v-if="props.locations && props.locations.length > 0">
+
+                        <ul class="mt-4 grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
+                            <li v-for="location in locations">
+                                <div v-if="location.title == event.title"></div>
+                                <div v-else>
+                                    <a :href="route('events.show', location)" class="block overflow-hidden">
+
+                                        <div class="group">
+                                            <img src="https://picsum.photos/1000?random=0" alt=""
+                                                class="h-[200px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[200px]" />
+                                            <h3
+                                                class="mt-2 text-sm text-gray-700 group-hover:underline group-hover:underline-offset-4">
+                                                {{ location.title }}
+                                            </h3>
+                                        </div>
+                                        <div class="relative bg-white pt-3">
+                                            <p>
+                                                <span class="tracking-wider text-gray-900"> {{ location.price }}$</span>
+                                            </p>
+                                            <p>
+                                                <span
+                                                    class="tracking-wider text-red-800 text-xs hover:underline hover:underline-offset-4">
+                                                    {{ location.location }} {{ location.attachedCategories }}
+                                                </span>
+                                            </p>
+                                        </div>
+                                    </a>
+                                </div>
+
+                            </li>
+                        </ul>
+                    </div>
+                    <p v-else> Aucun événement avec la même ville trouvé </p>
                 </div>
+
             </div>
 
 
