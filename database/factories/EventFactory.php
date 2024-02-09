55697df6ed6ee0decdcab28ac1c6a84c62a2d474
HugoDemont <hugodemont62@hotmail.fr>
2024-02-09 16:31:19 +0100

    [FEAT] Update Event Factory default dates
    This commit improves the event date generation in the Event Factory. It introduces start and end date randomization between the present time and 2, or 3 years out respectively. This adjustment ensures realistic event timeframes for testing.

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -16,11 +16,14 @@ class EventFactory extends Factory
      */
     public function definition(): array
     {
+        $startDate = $this->faker->dateTimeBetween('now', '+2 years');
+        $endDate = $this->faker->dateTimeBetween($startDate, '+3 years');
+
         return [
             'title' => $this->faker->sentence,
             'description' => $this->faker->paragraph,
-            'start_date' => $this->faker->dateTime,
-            'end_date' => $this->faker->dateTime,
+            'start_date' => $startDate,
+            'end_date' => $endDate,
             'location' => $this->faker->address,
             'capacity' => $this->faker->numberBetween(50, 200),
             'price' => $this->faker->numberBetween(0, 1000)
