dbfbd8b6a2c4e0eb733aa2d6b48298c6f8963547
HugoDemont <hugodemont62@hotmail.fr>
2024-02-17 11:19:35 +0100

    [FEAT] Add 'admin_id' foreign key to 'events' table
    The 'events' table in the database migration now includes a foreignId 'admin_id'. This foreign key references the 'users' table, enabling a better organization of entities and their relationships within the application.

--- database/migrations/2024_01_12_081943_create_events_table.php
+++ database/migrations/2024_01_12_081943_create_events_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->integer('capacity');
             $table->bigInteger('price')->default(0);
             $table->string('image')->nullable();
+            $table->foreignId('admin_id')->constrained('users');
             $table->timestamps();
         });
     }
