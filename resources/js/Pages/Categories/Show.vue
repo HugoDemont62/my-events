1b0f628e5bd384e18d354fdd59f6555209ecf4b7
HugoDemont <hugodemont62@hotmail.fr>
2024-02-17 22:54:03 +0100

    [FEAT] Add CategoriesController and related Vue.js components
    Created and implemented a new CategoriesController, along with accompanying Vue.js components Index.vue and Show.vue for displaying categories. The controller handles resource actions while the Vue components provide the UI for category listing and individual category display.

---
+++ resources/js/Pages/Categories/Show.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>{{ props.category.name }}</h1>
+        <ul>
+            <li v-for="(event, index) in props.events" :key="index">
+                {{ event.title }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    category: {
+        type: Object,
+        required: true,
+    },
+    events: {
+        type: Array,
+        required: true,
+    },
+});
+</script>
