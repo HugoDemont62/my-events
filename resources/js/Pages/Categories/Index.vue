8cdfa7599cfa59cc7aec0e995448ce0d441f12a9
honorine <honorineru@gmail.com>
2024-02-18 14:26:20 +0100

    Front sur la vue de toutes les catégories

--- resources/js/Pages/Categories/Index.vue
+++ resources/js/Pages/Categories/Index.vue
@@ -1,21 +1,5 @@
-<template>
-    <AppLayout>
-        <div>
-            <h1>Categories</h1>
-            <ul>
-                <li v-for="(category, index) in props.categories" :key="index">
-                    <a :href="route('categories.show', category)" class="block overflow-hidden">
-                        <img :src="`https://via.placeholder.com/150?text=${category.name}`" :alt="category.name"/>
-                        <p>{{ category.name }}</p>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </AppLayout>
-</template>
-
 <script setup>
-import {defineProps} from 'vue';
+import { defineProps } from 'vue';
 import AppLayout from '@/Layouts/AppLayout.vue';
 
 const props = defineProps({
@@ -25,3 +9,21 @@ const props = defineProps({
     },
 });
 </script>
+
+
+<template>
+    <AppLayout>
+        <div>
+            <h1 class="m-6 text-xl font-extrabold text-gray-900">Categories</h1>
+            <ul class="grid grid-cols-4 gap-4 m-4">
+                <li v-for="(category, index) in props.categories" :key="index">
+                    <a :href="route('categories.show', category)" class="block overflow-hidden">
+                        <img :src="`https://picsum.photos/1000?random=0`" :alt="category.name"
+                            class="h-[200px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[200px]" />
+                        <p>{{ category.name }}</p>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </AppLayout>
+</template>
